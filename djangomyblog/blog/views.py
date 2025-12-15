23e6f1e1b1e62cc41bc6142643a821589de7e543
André Luferat <contato@luferat.net>
2025-12-15 08:25:57 -0300

    ok

--- djangomyblog/blog/views.py
+++ djangomyblog/blog/views.py
@@ -1,10 +1,25 @@
 # djangomyblog\blog\views.py
 
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 def home(request):
     return render(request, "blog/home.html")
 
+
 def about(request):
     return render(request, "blog/about.html")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/signup.html", {"form": form})
