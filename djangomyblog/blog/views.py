398d373e088240c8a6bee12d2187916bfeb83e87
André Luferat <contato@luferat.net>
2025-12-17 08:30:25 -0300

    errrrrrroooo

--- djangomyblog/blog/views.py
+++ djangomyblog/blog/views.py
@@ -4,10 +4,21 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from djangomyblog.blog.models import Post
+
 from .forms import CustomUserCreationForm
 
+
 def home(request):
-    return render(request, "blog/home.html")
+    posts = (
+        Post.objects
+        .filter(status='ON')
+        .select_related('user')
+        .order_by('-created_at')
+    )
+
+    return render(request, "blog/home.html", {"posts": posts})
+
 
 def about(request):
     return render(request, "blog/about.html")
@@ -15,9 +26,11 @@ def about(request):
 # Só acessa profile() se estiver logado
 # Não está logado, vai para '/accounts/login/'
 
+
 @login_required
 def profile(request):
     return render(request, "blog/profile.html")
+
 
 def signup(request):
     if request.method == "POST":
